b5d79cb2480bb3b0831b33c954a346be4d974d7e
Koki Morii <cohki@cohki-macbooc-pro.lan>
2014-07-27 23:28:41 +0900

    first commit

--- SampleApp/main.m
+++ SampleApp/main.m
@@ -11,6 +11,8 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        NSLog(@"Hello, World!!!");
+        
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
